9308e0e9a990c4abd9a20ed9c446403a653949d3
Favio E. Pereyra <pereyrafavio@outlook.com.ar>
2024-08-16 07:41:59 -0300

    nuevo componente test2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ProyectA';
+
+constructor(){
+  const testMap =[1,2,3,4,5].map(item =>item *2);
+  console.log(testMap);
+
+
+const testForeach =[1,2,3,4,5].forEach(item =>item);
+console.log(testMap);
+
+
+}
+
+
 }
