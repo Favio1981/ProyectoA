4d8bde991eff507f79f6fad0e2f0eb2cb1155808
Favio E. Pereyra <pereyrafavio@outlook.com.ar>
2024-08-16 07:50:30 -0300

    input de tipo string test2

--- src/app/test2/test2.component.ts
+++ src/app/test2/test2.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-test2',
   templateUrl: './test2.component.html',
   styleUrls: ['./test2.component.scss']
 })
-export class Test2Component implements OnInit {
+export class Test2Component{
+
+
+  @Input() status: string;
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
 
 }
